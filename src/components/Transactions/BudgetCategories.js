9e2a159c3c0744ae4d8a2b2589ca0dc25651bece
Ryan's iMac <ryancmoret@gmail.com>
2019-01-02 16:57:07 -0600

    Render budget categories from subs, checknumber and styling

---
+++ src/components/Transactions/BudgetCategories.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class BudgetCategories extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      subtransactions: [],
+      ...props,
+    }
+    this.categories = this.categories.bind(this)
+  }
+
+  categories() {
+    if (this.state.subtransactions.length > 0) {
+      const arr = this.state.subtransactions.map((sub) => sub.budget_category)
+      return arr.join(', ')
+    } else {
+      return this.state.budget_category || ''
+    }
+  }
+
+  render() {
+    console.log(this.state)
+    if (this.categories().length > 0) {
+      return (
+        <div className="budget-categories">
+          {this.categories()}
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+}
+
+export default BudgetCategories;
--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BudgetCategories from './BudgetCategories'
 import Subtransaction from './Subtransaction'
 import LeftIcon from './LeftIcon'
 
@@ -6,7 +7,8 @@ class Transaction extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ...props
+      subtransactions: [],
+      ...props,
     }
     this.expandDetail = this.expandDetail.bind(this)
     this.collapseDetail = this.collapseDetail.bind(this)
@@ -54,12 +56,22 @@ class Transaction extends Component {
           <div className="balance">
             ${parseFloat(this.state.balance / 100.0).toFixed(2)}
           </div>
-          <div className="budget-categories">
-            {this.state.budget_category}
-          </div>
-          <span className="exclusion">
-            {this.state.budget_exclusion ? 'budget exclusion' : ''}
-          </span>
+          {this.state.check_number ?
+            <div className="check-number">
+              <i className="fas fa-money-check"></i>&nbsp;
+              Check Number: {this.state.check_number}
+            </div>
+            :
+          ''
+          }
+          <BudgetCategories {...this.state} />
+          {this.state.budget_exclusion ?
+            <div className="exclusion">
+              {this.state.budget_exclusion ? 'budget exclusion' : ''}
+            </div>
+            :
+          ''
+          }
           <div className="notes">
             {this.state.notes}
           </div>
