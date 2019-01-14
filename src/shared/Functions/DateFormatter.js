d45d42a9cae92c3a3ac4510fc964d68538bc6787
Ryan's iMac <ryancmoret@gmail.com>
2019-01-13 21:06:19 -0600

    linting in shared files

--- src/shared/Functions/DateFormatter.js
+++ src/shared/Functions/DateFormatter.js
@@ -1,24 +1,24 @@
 
 const DateFormatter = (dateString) => {
   const MonthDictionary = {
-    '01': 'Jan.',
-    '02': 'Feb.',
-    '03': 'Mar.',
-    '04': 'Apr.',
-    '05': 'May',
-    '06': 'June',
-    '07': 'July',
-    '08': 'Aug.',
-    '09': 'Sep.',
-    '10': 'Oct.',
-    '11': 'Nov.',
-    '12': 'Dec.',
+    "01": "Jan.",
+    "02": "Feb.",
+    "03": "Mar.",
+    "04": "Apr.",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "Aug.",
+    "09": "Sep.",
+    "10": "Oct.",
+    "11": "Nov.",
+    "12": "Dec.",
   }
 
-  const dateElements = dateString.split('-')
+  const dateElements = dateString.split("-")
   const month = MonthDictionary[dateElements[1]]
-  const day = parseInt(dateElements[2]) + ','
-  return [month, day, dateElements[0]].join(' ')
+  const day = parseInt(dateElements[2]) + ","
+  return [month, day, dateElements[0]].join(" ")
 }
 
-export default DateFormatter;
+export default DateFormatter
--- src/shared/Functions/MoneyFormatter.js
+++ src/shared/Functions/MoneyFormatter.js
@@ -1,10 +1,10 @@
 const MoneyFormatter = (number, opts = { absolute: false }) => {
   if (number === null) {
-    return ''
+    return ""
   } else {
     const num = opts.absolute ? Math.abs(number) : number
-    return '$' + (num / 100.0).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    return "$" + (num / 100.0).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
   }
 }
 
-export default MoneyFormatter;
+export default MoneyFormatter
