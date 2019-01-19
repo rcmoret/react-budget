d338cd60614918eae0e6887176cddbf6a145643f
Ryan's iMac <ryancmoret@gmail.com>
2019-01-18 18:23:08 -0600

    Make app a stateless component

--- src/App.js
+++ src/App.js
@@ -7,25 +7,21 @@ import BudgetCategories from './components/Budget/Categories';
 import Header from './components/Header'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path='/accounts' component={Accounts} />
-            <Route exact path='/accounts/index' component={AccountIndex} />
-            <Route path='/accounts/:id' component={Accounts} />
-            <Route exact path='/budget' component={BudgetIndex} />
-            <Route exact path='/budget/categories' component={BudgetCategories} />
-          </Switch>
-        </div>
-      </Router>
+const App = () => (
+  <div className="App">
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/accounts' component={Accounts} />
+          <Route exact path='/accounts/index' component={AccountIndex} />
+          <Route path='/accounts/:id' component={Accounts} />
+          <Route exact path='/budget' component={BudgetIndex} />
+          <Route exact path='/budget/categories' component={BudgetCategories} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </Router>
+  </div>
+)
 
 export default App;
