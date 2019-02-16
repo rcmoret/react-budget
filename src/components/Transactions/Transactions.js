28290ac0538c844f389cd921cc8d1e75984dc3cb
Ryan's iMac <ryancmoret@gmail.com>
2019-02-16 15:55:19 -0600

    Map state => props in transactions

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -5,67 +5,70 @@ import Transaction from "./Transaction";
 import * as DateFormatter from "../../shared/Functions/DateFormatter"
 
 const Transactions = (props) => {
-  const dateRange = () => {
-    let str = "Transactions from: "
-    str += DateFormatter.fromDateString(props.metadata.date_range[0])
-    str += " to "
-    str += DateFormatter.fromDateString(props.metadata.date_range[1])
-    return str
-  }
-
-  const orderedTransactions = () => {
-    const date = new Date()
-    const today = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().slice(0,10)
-    return props.collection.sort(function(a, b) {
-      if (a.clearance_date === b.clearance_date) {
-        return 0
-      } else if (a.clearance_date === null) {
-        return b.clearance_date > today ? -1 : 1
-      } else if (b.clearance_date === null) {
-        return a.clearance_date > today ? 1 : -1
-      } else {
-        // equailty is handled above
-        return (a.clearance_date > b.clearance_date) ? 1 : -1
-      }
-    })
-  }
-
-  const transactionsWithBal = () => {
-    let balance = props.metadata.prior_balance
-    const arr = orderedTransactions().map((transaction) => {
-      balance += transaction.amount
-      return { balance: balance, ...transaction }
-    })
-    return arr
-  }
-
-  const initialTransaction = () => {
-    return {
-      balance: props.metadata.prior_balance,
-      clearance_date: props.metadata.date_range[0],
-      amount: null,
-      description: 'Balance',
-      subtransactions: [],
-    }
-  }
+  const dateRange = `Transactions from: ${props.startDate} to ${props.endDate}`
 
   return (
     <div className="transactions">
       <h2>Transactions</h2>
       <div className='transaction-metadata'>
-        {dateRange()}
+        {dateRange}
       </div>
       <hr/>
-      <Transaction key='0' {...initialTransaction()} />
-      {transactionsWithBal().map((transaction) =>
+      <Transaction key='0' {...props.initialTransaction} />
+      {props.collection.map(transaction =>
         <Transaction
           key={transaction.id}
           {...transaction}
         />
       )}
-    <AddTransaction {...props} />
+    <AddTransaction />
     </div>
   )
 }
 
-export default connect(state => state.transactions)(Transactions)
+const mapStateToProps = (state) => {
+  const { collection, metadata } = state.transactions
+  const startDate = DateFormatter.fromDateString(metadata.date_range[0])
+  const endDate =  DateFormatter.fromDateString(metadata.date_range[1])
+  const date = new Date()
+  const today = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().slice(0,10)
+
+  const orderedTransactions = collection.sort(function(a, b) {
+    if (a.clearance_date === b.clearance_date) {
+      return 0
+    } else if (a.clearance_date === null) {
+      return b.clearance_date > today ? -1 : 1
+    } else if (b.clearance_date === null) {
+      return a.clearance_date > today ? 1 : -1
+    } else {
+      // equailty is handled above
+      return (a.clearance_date > b.clearance_date) ? 1 : -1
+    }
+  })
+
+  const collectionWithBalance = () => {
+    let balance = metadata.prior_balance
+    return collection.map(transaction => {
+      balance += transaction.amount
+      return { balance: balance, ...transaction }
+    })
+  }
+
+  const initialTransaction = {
+    balance: metadata.prior_balance,
+    clearance_date: metadata.date_range[0],
+    amount: null,
+    description: 'Balance',
+    subtransactions: [],
+  }
+
+  return {
+    startDate: startDate,
+    endDate: endDate,
+    initialTransaction: initialTransaction,
+    collection: collectionWithBalance(),
+    ...state.transactions,
+  }
+}
+
+export default connect(mapStateToProps)(Transactions)
