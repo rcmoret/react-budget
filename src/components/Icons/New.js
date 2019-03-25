fca0eab848ed0f9fd4dade474d0e62a5a3b3b39a
Ryan's iMac <ryancmoret@gmail.com>
2019-03-25 10:00:53 -0500

    Clean up new icon

--- src/components/Icons/New.js
+++ src/components/Icons/New.js
@@ -7,21 +7,21 @@ import Form from "./Form/Form"
 const New = (props) => {
   const onChange = (e) => {
     const action = updateNew({[e.target.name]: e.target.value })
-    this.props.dispatch(action)
+    props.dispatch(action)
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = () => {
     const url = ApiUrlBuilder(["icons"])
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(props.newIcon),
-  })
-  .then(resp => resp.json())
-  .then(data => props.dispatch(created(data)))
+    })
+      .then(resp => resp.json())
+      .then(data => props.dispatch(created(data)))
   }
 
   return (
@@ -34,7 +34,7 @@ const New = (props) => {
   )
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return { newIcon: state.icons.newIcon }
 }
 
