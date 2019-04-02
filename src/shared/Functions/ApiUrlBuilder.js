37ca54182d2399065466533a3aa8c1f14c214c26
Ryan's iMac <ryancmoret@gmail.com>
2019-04-02 09:41:27 -0500

    Change url builder

--- src/shared/Functions/ApiUrlBuilder.js
+++ src/shared/Functions/ApiUrlBuilder.js
@@ -2,7 +2,8 @@ import API_URL from "../Constants/Api"
 
 const ApiUrlBuilder = (path_segments = [], params = {}) => {
   const query = Object.entries(params).map((arr) => { return arr.join("=") }).join("&")
-  return `${[API_URL, ...path_segments].join("/")}?${query}`
+  const url = [API_URL, ...path_segments].join("/")
+  return query === "" ? url : `${url}?${query}`
 }
 
 export default ApiUrlBuilder
