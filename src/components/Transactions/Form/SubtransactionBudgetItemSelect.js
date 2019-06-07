ac3db86350ed597059fbf8faa9ce1fe6d2bec9c4
rcmoret <ryancmoret@gmail.com>
2019-06-06 22:39:19 -0400

    Show budget category on subtransaction edit

--- src/components/Transactions/Form/SubtransactionBudgetItemSelect.js
+++ src/components/Transactions/Form/SubtransactionBudgetItemSelect.js
@@ -2,17 +2,20 @@ import React from "react"
 
 import Select from "react-select"
 
-export default ({ _id, budget_item_id, options, onSubChange }) => {
+export default (props) => {
+  const { _id, budget_category, budget_item_id, options, onSubChange } = props
   const onChange = (e) => {
     onSubChange(_id, { budget_item_id: e.value })
   }
-  const value = options.find(option => option.value === budget_item_id)
+  const value = { label: budget_category, value: budget_item_id }
+  const emptyValue = { label: "Discretionary", value: null }
+  const selectedValue = budget_category ? value : emptyValue
 
   return (
     <div className="subtransaction-budget-item-select">
       <Select
         options={options}
-        value={value}
+        value={selectedValue}
         onChange={onChange}
         className="budget-item-select-container"
         classNamePrefix="budget-item-select"
