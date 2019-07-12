ec1b61bd8f0b67b255dffc54367ad72c65ed2bcb
Ryan's iMac <ryancmoret@gmail.com>
2019-07-12 16:26:16 -0500

    Refactor how item form is handled

--- src/components/Budget/Items/MonthlyItemForm.js
+++ src/components/Budget/Items/MonthlyItemForm.js
@@ -7,98 +7,96 @@ import { decimalToInt } from "../../../shared/Functions/MoneyFormatter"
 import Select from "react-select";
 
 
-class MonthlyItemForm extends Component {
-  constructor(props) {
-    super(props)
-    this.onCategoryChange = this.onCategoryChange.bind(this)
-    this.onAmountChange = this.onAmountChange.bind(this)
-    this.onSave = this.onSave.bind(this)
+const MonthlyItemForm = (props) => {
+  const {
+    amount,
+    budget_category_id,
+    categories,
+    fetched,
+    dispatch,
+    month,
+    options,
+    showForm,
+    value,
+    year,
+  } = props
+
+  if (!fetched) {
+    const url = ApiUrlBuilder(["budget/categories"])
+    fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch(categoriesFetched(data)))
   }
 
-  componentDidUpdate() {
-    if (this.props.showForm && !this.props.categoriesFetched) {
-      const url = ApiUrlBuilder(['budget', 'categories'])
-      fetch(url)
-        .then(response => response.json())
-        .then(data => this.props.dispatch(categoriesFetched(data)))
-    }
-  }
-
-  onCategoryChange(e) {
-    const category = this.props.categories.find(category => category.id === e.value)
-    this.props.dispatch(editNewMonthlyItem({
+  const onCategoryChange = (e) => {
+    const category = categories.find(category => category.id === e.value)
+    const action = editNewMonthlyItem({
       budget_category_id: e.value,
       amount: parseFloat(category.default_amount / 100.0).toFixed(2)
-    }))
+    })
+    dispatch(action)
   }
 
-  onAmountChange(e) {
-    this.props.dispatch(editNewMonthlyItem({
-      amount: e.target.value
-    }))
+  const onAmountChange = (e) => {
+    const action = editNewMonthlyItem({ amount: e.target.value })
+    dispatch(action)
   }
 
-  onSave(e) {
+  const onSave = (e) => {
     e.preventDefault()
-    const { amount, budget_category_id, month, year } = this.props
     const body = {
+      amount: decimalToInt(amount),
       month: month,
       year: year,
-      amount: decimalToInt(amount),
-      budget_category_id: budget_category_id,
     }
-    const url = ApiUrlBuilder(["budget", "categories", budget_category_id, "items"])
+    const url = ApiUrlBuilder(["budget/categories", budget_category_id, "items"])
     fetch(url, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        "Accept": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(body)
     })
-    .then(response => response.json())
-    .then(data => this.props.dispatch(
-      addMonthlyItem(data)
-    ))
-    .then(() => {
-      this.props.dispatch(toggleMonthlyItemForm({ showForm: false }))
-      this.props.dispatch(editNewMonthlyItem({ amount: "", budget_category_id: null }))
-    })
+      .then(response => response.json())
+      .then(data => dispatch(addMonthlyItem(data)))
+      .then(() => {
+        dispatch(toggleMonthlyItemForm({ showForm: false }))
+        dispatch(editNewMonthlyItem({ amount: "", budget_category_id: null }))
+      })
   }
 
-  render() {
-    if (this.props.showForm) {
-      return (
-        <div className="new-budget-item">
-          <div className="select-wrapper">
-            <Select
-              value={this.props.value}
-              options={this.props.options}
-              onChange={this.onCategoryChange}
-              className="budget-category-select-container"
-              classNamePrefix="budget-category-select"
-            />
-          </div>
-          <input
-            name="amount"
-            className="new-item-amount"
-            placeholder="amount"
-            value={this.props.amount}
-            onChange={this.onAmountChange}
+  if (showForm) {
+    return (
+      <div className="new-budget-item">
+        <div className="select-wrapper">
+          <Select
+            value={props.value}
+            options={props.options}
+            onChange={onCategoryChange}
+            className="budget-category-select-container"
+            classNamePrefix="budget-category-select"
           />
-          <button
-            className="new-item-submit"
-            type="submit"
-            name="monthly-item-submit"
-            onClick={this.onSave}
-          >
-            CREATE
-          </button>
         </div>
-      )
-    } else {
-      return null
-    }
+        <input
+          name="amount"
+          className="new-item-amount"
+          placeholder="amount"
+          value={amount}
+          onChange={onAmountChange}
+        />
+        <button
+          className="new-item-submit"
+          type="submit"
+          name="monthly-item-submit"
+          onClick={onSave}
+        >
+          CREATE
+        </button>
+      </div>
+    )
+  } else {
+    return null
   }
 }
 
--- src/components/Budget/Items/WeeklyItemForm.js
+++ src/components/Budget/Items/WeeklyItemForm.js
@@ -6,46 +6,49 @@ import { connect } from "react-redux"
 import { decimalToInt } from "../../../shared/Functions/MoneyFormatter"
 import Select from "react-select"
 
-class WeeklyItemForm extends Component {
-  constructor(props) {
-    super(props)
-    this.onCategoryChange = this.onCategoryChange.bind(this)
-    this.onAmountChange = this.onAmountChange.bind(this)
-    this.onSave = this.onSave.bind(this)
+const WeeklyItemForm = (props) => {
+  const {
+    amount,
+    budget_category_id,
+    categories,
+    fetched,
+    dispatch,
+    month,
+    options,
+    showForm,
+    value,
+    year,
+  } = props
+
+  if (!fetched) {
+    const url = ApiUrlBuilder(["budget/categories"])
+    fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch(categoriesFetched(data)))
   }
 
-  componentDidUpdate() {
-    if (this.props.showForm && !this.props.categoriesFetched) {
-      const url = ApiUrlBuilder(["budget", "categories"])
-      fetch(url)
-        .then(response => response.json())
-        .then(data => this.props.dispatch(categoriesFetched(data)))
-    }
-  }
-
-  onCategoryChange(e) {
-    const category = this.props.categories.find(category => category.id === e.value)
-    this.props.dispatch(editNewWeeklyItem({
+  const onCategoryChange = (e) => {
+    const category = categories.find(category => category.id === e.value)
+    const action = editNewWeeklyItem({
       budget_category_id: e.value,
       amount: parseFloat(category.default_amount / 100.0).toFixed(2)
-    }))
+    })
+    dispatch(action)
   }
 
-  onAmountChange(e) {
-    this.props.dispatch(editNewWeeklyItem({
-      amount: e.target.value
-    }))
+  const onAmountChange = (e) => {
+    const action = editNewWeeklyItem({ amount: e.target.value })
+    dispatch(action)
   }
 
-  onSave(e) {
+  const onSave = (e) => {
     e.preventDefault()
-    const { amount, budget_category_id, month, year } = this.props
     const body = {
+      amount: decimalToInt(amount),
       month: month,
       year: year,
-      amount: decimalToInt(amount),
     }
-    const url = ApiUrlBuilder(["budget", "categories", budget_category_id, "items"])
+    const url = ApiUrlBuilder(["budget/categories", budget_category_id, "items"])
     fetch(url, {
       method: "POST",
       headers: {
@@ -55,48 +58,44 @@ class WeeklyItemForm extends Component {
       body: JSON.stringify(body)
     })
       .then(response => response.json())
-      .then(data => this.props.dispatch(
-        addWeeklyItem(data)
-      ))
+      .then(data => dispatch(addWeeklyItem(data)))
       .then(() => {
-        this.props.dispatch(toggleWeeklyItemForm({ showForm: false }))
-        this.props.dispatch(editNewWeeklyItem({ amount: "", budget_category_id: null }))
+        dispatch(toggleWeeklyItemForm({ showForm: false }))
+        dispatch(editNewWeeklyItem({ amount: "", budget_category_id: null }))
       })
   }
 
-  render() {
-    if (this.props.showForm) {
-      return (
-        <div className="new-budget-item">
-          <div className="select-wrapper">
-            <Select
-              value={this.props.value}
-              options={this.props.options}
-              onChange={this.onCategoryChange}
-              className="budget-category-select-container"
-              classNamePrefix="budget-category-select"
-            />
-          </div>
-          <input
-            name="amount"
-            className="new-item-amount"
-            placeholder="amount"
-            value={this.props.amount}
-            onChange={this.onAmountChange}
+  if (showForm) {
+    return (
+      <div className="new-budget-item">
+        <div className="select-wrapper">
+          <Select
+            value={value}
+            options={options}
+            onChange={onCategoryChange}
+            className="budget-category-select-container"
+            classNamePrefix="budget-category-select"
           />
-          <button
-            className="new-item-submit"
-            type="submit"
-            name="weekly-item-submit"
-            onClick={this.onSave}
-          >
-            CREATE
-          </button>
         </div>
-      )
-    } else {
-      return null
-    }
+        <input
+          name="amount"
+          className="new-item-amount"
+          placeholder="amount"
+          value={amount}
+          onChange={onAmountChange}
+        />
+        <button
+          className="new-item-submit"
+          type="submit"
+          name="weekly-item-submit"
+          onClick={onSave}
+        >
+          CREATE
+        </button>
+      </div>
+    )
+  } else {
+    return null
   }
 }
 
@@ -118,14 +117,14 @@ const mapStateToProps = (state) => {
   const value = options.find(option => option.value === newItem.budget_category_id)
   const { month, year } = state.budget.metadata
   return {
-    options: options,
-    categories: categories,
-    showForm: state.budget.weekly.showForm,
-    categoriesFetched: state.budget.categories.fetched,
-    value: value,
     amount: newItem.amount,
+    categories: categories,
     budget_category_id: newItem.budget_category_id,
+    fetched: state.budget.categories.fetched,
     month: month,
+    options: options,
+    showForm: state.budget.weekly.showForm,
+    value: value,
     year: year,
   }
 }
