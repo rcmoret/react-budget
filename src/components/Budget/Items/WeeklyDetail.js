6aea03ad88b5228001394dde317764385a0c84a9
Ryan's iMac <ryancmoret@gmail.com>
2019-07-20 15:55:08 -0500

    Weekly Detail cleanup

--- src/components/Budget/Items/WeeklyDetail.js
+++ src/components/Budget/Items/WeeklyDetail.js
@@ -1,48 +1,57 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
+
 import ApiUrlBuilder from "../../../functions/ApiUrlBuilder"
 import { fetchedWeeklyTransactions } from "../../../actions/budget"
+
 import Details from "../Shared/Details"
 import Transactions from "../Shared/Transactions"
 
-class WeeklyDetail extends Component {
-  componentDidUpdate() {
-    const { budget_category_id, collection, id, showDetail, transaction_count } = this.props
+const WeeklyDetail = (props) => {
+  const {
+    budget_category_id,
+    budgetedPerDay,
+    budgetedPerWeek,
+    collection,
+    dispatch,
+    id,
+    name,
+    remainingPerDay,
+    remainingPerWeek,
+    showDetail,
+    transaction_count,
+  } = props
+
+  if (collection.length < transaction_count && showDetail) {
     const url = ApiUrlBuilder(
       ["budget", "categories", budget_category_id, "items", id, "transactions"]
     )
-    if (collection.length < transaction_count && showDetail) {
-      fetch(url)
-        .then(response => response.json())
-        .then(data => this.props.dispatch(
-          fetchedWeeklyTransactions({ id: id, collection: data }))
-        )
-    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch(
+        fetchedWeeklyTransactions({ id: id, collection: data }))
+      )
   }
 
-  render() {
-    if (this.props.showDetail) {
-      const { budgetedPerDay, budgetedPerWeek, collection,
-              name, remainingPerDay, remainingPerWeek } = this.props
-      return (
-        <div className="detail-wrapper">
-          <hr />
-          <Details
-            budgetedPerDay={budgetedPerDay}
-            budgetedPerWeek={budgetedPerWeek}
-            remainingPerDay={remainingPerDay}
-            remainingPerWeek={remainingPerWeek}
-          />
-          <hr />
-          <Transactions
-            budgetCategory={name}
-            collection={collection}
-          />
-        </div>
-      )
-    } else {
-      return null
-    }
+  if (showDetail) {
+    return (
+      <div className="detail-wrapper">
+        <hr />
+        <Details
+          budgetedPerDay={budgetedPerDay}
+          budgetedPerWeek={budgetedPerWeek}
+          remainingPerDay={remainingPerDay}
+          remainingPerWeek={remainingPerWeek}
+        />
+        <hr />
+        <Transactions
+          budgetCategory={name}
+          collection={collection}
+        />
+      </div>
+    )
+  } else {
+    return null
   }
 }
 
