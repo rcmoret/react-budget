13873179227e000a360cdb94186a1052b8605b5d
Ryan's iMac <ryancmoret@gmail.com>
2019-08-23 20:54:10 -0500

    Last component to pure function

--- src/components/Transactions/Wrapper.js
+++ src/components/Transactions/Wrapper.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
 
 import { fetchedTransactions } from "../../actions/transactions"
@@ -8,35 +8,44 @@ import { get } from "../../functions/ApiClient"
 
 import Transactions from "./Transactions"
 
-class Wrapper extends Component {
-  fetchTransactions(accountId, monthYear) {
-    if (!Number.isInteger(accountId)) {
-      return
-    } else {
-      const url = ApiUrlBuilder(
-        ["accounts", accountId, "transactions"], { ...monthYear }
-      )
-      get(url, data => this.props.dispatch(fetchedTransactions(data)))
-    }
+const Wrapper = (props) => {
+  const {
+    accountId,
+    dispatch,
+    month,
+    urlAccountId,
+    urlMonth,
+    urlYear,
+    year,
+  } = props
+
+  if (!Number.isInteger(urlAccountId)) {
+    return null
   }
 
-  componentWillMount() {
-    const { month, year } = this.props
-    this.fetchTransactions(this.props.accountId, { month: month, year: year })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { month, year } = nextProps
-    this.fetchTransactions(nextProps.accountId, { month: month, year: year })
-  }
-
-  render() {
-    return (
-      <Transactions {...this.props} />
+  if (urlMonth !== month || urlYear !== year || urlAccountId !== accountId) {
+    const url = ApiUrlBuilder(
+      ["accounts", urlAccountId, "transactions"], { month: urlMonth, year: urlYear }
     )
+    get(url, data => dispatch(fetchedTransactions(data)))
   }
+
+  return (
+    <Transactions {...props} />
+  )
 }
 
-const mapStateToProps = (_state, ownProps) => ownProps
+const mapStateToProps = (state, ownProps) => {
+  const { query_options } = state.transactions.metadata
+
+  return {
+    accountId: parseInt(query_options.account_id),
+    month: parseInt(query_options.month),
+    urlAccountId: ownProps.accountId,
+    urlMonth: ownProps.month,
+    urlYear: ownProps.year,
+    year: parseInt(query_options.year),
+  }
+}
 
 export default connect(mapStateToProps)(Wrapper)
