1d10f66632dc14387c4467e21e5fadd19c162d76
Ryan's iMac <ryancmoret@gmail.com>
2019-10-28 17:37:14 -0500

    Fix monthly item model

--- src/shared/models/monthlyBudgetItem.js
+++ src/shared/models/monthlyBudgetItem.js
@@ -3,19 +3,19 @@ export default (attributes) => {
     accrual,
     amount,
     expense,
-    maturity_month,
-    maturity_year,
+    maturityMonth,
+    maturityYear,
     month,
     year,
   } = attributes
   const spent = attributes.spent || 0
-  const transaction_count = attributes.transaction_count || 0
+  const transactionCount = attributes.transactionCount || 0
   const difference = amount - spent
-  const deletable = transaction_count === 0
+  const deletable = transactionCount === 0
   const remaining = deletable ? amount : 0
   const overUnderBudget = (expense && difference > 0) || (!expense && difference < 0)
   const overUnderBudgetAmount = !deletable ? (-1 * difference) : 0
-  const matureAccrual = accrual && maturity_month === month && maturity_year === year
+  const matureAccrual = accrual && maturityMonth === month && maturityYear === year
 
   return {
     ...attributes,
