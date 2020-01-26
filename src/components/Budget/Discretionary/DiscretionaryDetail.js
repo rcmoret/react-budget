5271f74101482b64abc5a4cb347efa8f674a2ad6
rcmoret <ryancmoret@gmail.com>
2020-01-26 12:07:44 -0500

    Order discretionary transactions

--- src/components/Budget/Discretionary/DiscretionaryDetail.js
+++ src/components/Budget/Discretionary/DiscretionaryDetail.js
@@ -56,7 +56,26 @@ const DiscretionaryDetail = (props) => {
 }
 
 const mapStateToProps = (state) => {
-  return { ...state.budget.metadata, ...state.budget.discretionary }
+  const date = new Date()
+  const today = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().slice(0,10)
+  const collection = state.budget.discretionary.collection.sort((a, b) => {
+    if (a.clearance_date === b.clearance_date) {
+      return 0
+    } else if (a.clearance_date === null) {
+      return b.clearance_date > today ? -1 : 1
+    } else if (b.clearance_date === null) {
+      return a.clearance_date > today ? 1 : -1
+    } else {
+      // equailty is handled above
+      return (a.clearance_date > b.clearance_date) ? 1 : -1
+    }
+  })
+
+  return {
+    ...state.budget.metadata,
+    ...state.budget.discretionary,
+    collection: collection
+  }
 }
 
 export default connect(mapStateToProps)(DiscretionaryDetail)
